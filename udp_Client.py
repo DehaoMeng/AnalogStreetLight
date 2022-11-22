b45035db32fd8d81d297e05e503a000ca8f77f08
DeHaoMeng <89242548+ITApeDeHao@users.noreply.github.com>
2022-11-22 18:46:24 +0800

    Update udp_Client.py

--- udp_Client.py
+++ udp_Client.py
@@ -2,7 +2,9 @@
 
 import socket
 from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, Button, messagebox
+
 LocalHost = "服务端ip地址"
+
 
 class Client:
     client_socket = None
@@ -32,7 +34,6 @@ class Gui:
         self.display_light_b()
         self.display_light_c()
 
-
     def status(self):
         Label(self.root, text=" 路灯终端A ", font=('heiti', 15), width=10, height=2).place(x=100, y=50)
         Label(self.root, text=" 状 态  ", font=('heiti', 30), width=7, height=2).place(x=200, y=37)
@@ -43,7 +44,6 @@ class Gui:
         # def server(self):
         # Button(self.root,text=" 开 始 服 务",font=('heiti',8),width=13,height=1,relief="groove").place(x=10,y=10)
         # Button(self.root,text=" 终 止 服 务 ",font=('heiti',8),width=13,height=1,relief="groove").place(x=10,y=27)
-
 
     def display_light_a(self):
         frame = Frame(self.root, height=self.height, width=self.width, bd=20, relief="groove")
@@ -58,13 +58,14 @@ class Gui:
         self.l_a = Entry(frame, width=10)
         self.l_a.place(x=60, y=110)
         Button(frame, text=" 上 传 数 据", font=('heiti', 8), width=13, height=2, relief="groove",
-               command=lambda: self.send_message(self.t_a, self.w_a, self.l_a, frame)).place(x=200, y=55)
+               command=lambda t_a=self.t_a, w_a=self.w_a, l_a=self.l_a, frame=frame: self.send_message(t_a, w_a, l_a,
+                                                                                                       frame)).place(
+            x=200, y=55)
         # self.status_light()
         # 接收数据
         # 根据收到的数据改变当前状态
         # Label(frame,text=" 状态：",font=('heiti',30),width=7,height=2).place(x=200,y=37)
         # Entry(frame,width=4).place(x=310,y=75)
-
 
     def display_light_b(self):
         frame = Frame(self.root, height=self.height, width=self.width, bd=20, relief="groove")
@@ -79,12 +80,13 @@ class Gui:
         self.l_b = Entry(frame, width=10)
         self.l_b.place(x=60, y=110)
         Button(frame, text=" 上 传 数 据", font=('heiti', 8), width=13, height=2, relief="groove",
-               command=lambda: self.send_message(self.t_b, self.w_b, self.l_b, frame)).place(x=200, y=55)
+               command=lambda t_b=self.t_b, w_b=self.w_b, l_b=self.l_b, frame=frame: self.send_message(t_b, w_b, l_b,
+                                                                                                       frame)).place(
+            x=200, y=55)
         # 接收数据
         # 根据收到的数据改变当前状态
         # Label(frame,text=" 状态：",font=('heiti',30),width=7,height=2).place(x=200,y=37)
         # Entry(frame,width=4).place(x=310,y=75)
-
 
     def display_light_c(self):
         frame = Frame(self.root, height=self.height, width=self.width, bd=20, relief="groove")
@@ -98,9 +100,11 @@ class Gui:
         self.w_c.place(x=60, y=70)
         self.l_c = Entry(frame, width=10)
         self.l_c.place(x=60, y=110)
-        Button(frame, text=" 上 传 数 据", font=('heiti', 8), width=13, height=2, relief="groove", command=lambda: self.send_message(self.t_c, self.w_c, self.l_c, frame)).place(x=200, y=55)
+        Button(frame, text=" 上 传 数 据", font=('heiti', 8), width=13, height=2, relief="groove",
+               command=lambda t_c=self.t_c, w_c=self.w_c, l_c=self.l_c, frame=frame: self.send_message(t_c, w_c, l_c,
+                                                                                                       frame)).place(
+            x=200, y=55)
         # Label(frame,text=" 状态：",font=('heiti',30),width=7,height=2).place(x=200,y=37)
-
 
     def recvfrom_server(self, frame):
         self.status, self.address = self.client.clientsocket.recvfrom(1024)
@@ -108,7 +112,6 @@ class Gui:
             Label(frame, text="OPEN", font=('heiti', 10), width=5, height=2, relief="groove").place(x=300, y=55)
         else:
             Label(frame, text="CLOSE", font=('heiti', 10), width=5, height=2, relief="groove").place(x=300, y=55)
-
 
     def send_message(self, t, w, l, frame):
         self.client.initialize_socket()
@@ -122,7 +125,6 @@ class Gui:
         if data_l == '#' or data_w == 'w' or data_t == '#':
             self.on_close_window()
         self.recvfrom_server(frame)
-
 
     def on_close_window(self):
         if messagebox.askokcancel("退出", "确定退出吗?"):
